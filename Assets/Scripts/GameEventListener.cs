b8604c02f54d12566808864dd559fbc065606a5e
Greger Sundvall <gsundvall@gmail.com>
2021-12-17 13:07:37 +0100

    asdf

--- Assets/Scripts/GameEventListener.cs
+++ Assets/Scripts/GameEventListener.cs
@@ -5,12 +5,12 @@ using UnityEngine.Events;
 
 public class GameEventListener : MonoBehaviour
 {
-    [SerializeField] private GameEvent _gameEvent;
-    [SerializeField] private UnityEvent _unityEvent;
+    [SerializeField] protected GameEvent _gameEvent;
+    [SerializeField] protected UnityEvent _unityEvent;
 
     void Awake() => _gameEvent.Register(this);
 
     void OnDestroy() => _gameEvent.Deregister(this);
 
-    public void RaiseEvent() => _unityEvent.Invoke();
+    public virtual void RaiseEvent() => _unityEvent.Invoke();
 }
---
+++ Assets/Scripts/GameEventListenerWithDelay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class GameEventListenerWithDelay : GameEventListener
+{
+    [SerializeField] private float _delay = 0.2f;
+    [SerializeField] private UnityEvent _delayedUnityEvent;
+    
+    public override void RaiseEvent()
+    {
+        _unityEvent.Invoke();
+        StartCoroutine(RunDelayedEvent());
+    }
+
+    private IEnumerator RunDelayedEvent()
+    {
+        yield return new WaitForSeconds(_delay);
+        _delayedUnityEvent.Invoke();
+    }
+}
